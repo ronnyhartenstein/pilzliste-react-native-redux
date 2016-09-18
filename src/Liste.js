b790b036f66a6eb728a9978e91744a0f9f917896
Ronny Hartenstein <ronny@rh-flow.de>
2016-09-18 23:03:52 +0200

    Liste per List und ListItem statt ListView

--- src/Liste.js
+++ src/Liste.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react'
 import { View, ScrollView, ListView, StyleSheet, Text } from 'react-native'
+import { List, ListItem } from 'react-native-elements'
 
 import daten from '../daten/pilze.json'
 
 class Liste extends Component {
   constructor(props) {
     super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      dataSource: ds.cloneWithRows(daten)
-    };
+    // const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    // this.state = {
+    //   dataSource: ds.cloneWithRows(daten)
+    // };
   }
   render() {
     return (
       <ScrollView style={styles.liste}>
-        <ListView
-        dataSource={this.state.dataSource}
-        renderRow={(rowData) => <Text>{rowData.name}</Text>}
-        />
+        <List containerStyle={{marginBottom: 20}}>
+            {
+                daten.map((l, i) => (
+                    <ListItem
+                        key={i}
+                        title={l.name}
+                    />
+                ))
+            }
+        </List>
       </ScrollView>
     );
   }
@@ -26,8 +33,9 @@ class Liste extends Component {
 const styles = StyleSheet.create({
   liste: {
     flex: 1, 
-    backgroundColor: 'skyblue',
-    paddingTop: 22
+    //backgroundColor: 'skyblue',
+    //marginTop: 0,
+    //paddingTop: 0
   }
 });
 
