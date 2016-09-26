e27025297d2cb02f37da74e57df85c5d0f8336f9
Ronny Hartenstein <ronny@rh-flow.de>
2016-09-26 22:27:50 +0200

    Memoized Selectors: filtert, aber Liste wird nicht neu gerendert

--- src/components/ReduxListe.js
+++ src/components/ReduxListe.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react'
 import Liste from './Liste'
 import { connect } from 'react-redux'
-// import FuzzySearch from 'fuzzy-search'
+import { createSelector } from 'reselect'
 import _ from 'lodash'
 
 // Memoized Selectors -> http://redux.js.org/docs/recipes/ComputingDerivedData.html
 
-const getVisibleItems = (items, search) => {
-  if (_.isEmpty(search) || search.length < 3) {
-    return { filteredItems: items }
-  } else {
-    const r = new RegExp(search, 'i')
-    const filtered = _.filter(items, itm => ( r.test(itm.name) || r.test(itm.lat) ))
-    console.log("Filtered: ", filtered)
-    return filtered
+const getSearch = (state) => state.search
+const getItems = (state) => state.items
+
+const getVisibleItems = createSelector(
+  [ getSearch, getItems ],
+  (search, items) => {
+    // if (_.isEmpty(search) || search.length < 3) {
+    //   return items
+    // } else {
+      const r = new RegExp(search, 'i')
+      const filtered = _.filter(items, itm => ( r.test(itm.name) || r.test(itm.lat) ))
+      console.log("Filtered: ", filtered)
+      return filtered
+    // }
   }
-}
+)
 
 const mapStateToProps = (state) => {
   return {
-    filteredItems: getVisibleItems(state.items, state.search)
+    filteredItems: getVisibleItems(state)
   } 
 }
 
