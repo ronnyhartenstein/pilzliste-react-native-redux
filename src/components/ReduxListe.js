639a36cadc667108a4acc84cdc2a1e4a913fece5
Ronny Hartenstein <ronny@rh-flow.de>
2016-09-26 22:20:21 +0200

    Memoized Selectors: zunächst ausgelagerte Funktion

--- src/components/ReduxListe.js
+++ src/components/ReduxListe.js
@@ -4,21 +4,23 @@ import { connect } from 'react-redux'
 // import FuzzySearch from 'fuzzy-search'
 import _ from 'lodash'
 
-const mapStateToProps = (state) => {
-  console.log("from state", state)
-  // https://www.npmjs.com/package/fuzzy-search
-  // Error "couldn't find preset "es2015" relative to directory.."
-//   const searchengine = () => ( new FuzzySearch(_.cloneDeep(state.items), ['name','lat']) );
-  
-//   console.log("INIT Liste Redux", state)
-// if (_.isEmpty(state.search) || state.search.length < 3) {
-//     return { filteredItems: state.items }
-//   } else {
-    const r = new RegExp(state.search, 'i')
-    const filtered = _.filter(state.items, itm => ( r.test(itm.name) || r.test(itm.lat) ))
+// Memoized Selectors -> http://redux.js.org/docs/recipes/ComputingDerivedData.html
+
+const getVisibleItems = (items, search) => {
+  if (_.isEmpty(search) || search.length < 3) {
+    return { filteredItems: items }
+  } else {
+    const r = new RegExp(search, 'i')
+    const filtered = _.filter(items, itm => ( r.test(itm.name) || r.test(itm.lat) ))
     console.log("Filtered: ", filtered)
-    return { filteredItems: filtered }
-  // }
+    return filtered
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    filteredItems: getVisibleItems(state.items, state.search)
+  } 
 }
 
 const mapDispatchToProps = (dispatch) => {
