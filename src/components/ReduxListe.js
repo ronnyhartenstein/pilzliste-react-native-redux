4a67a83db689d95283c06509865c6f90cb7a8b2d
Ronny Hartenstein <ronny@rh-flow.de>
2016-09-28 20:33:10 +0200

    normale Suche reintegriert

--- src/components/ReduxListe.js
+++ src/components/ReduxListe.js
@@ -3,7 +3,6 @@ import Liste from './Liste'
 import { connect } from 'react-redux'
 import { createSelector } from 'reselect'
 import _ from 'lodash'
-import fuzzy from 'fuzzy'
 
 // Memoized Selectors: http://redux.js.org/docs/recipes/ComputingDerivedData.html
 // Fuzzy Search: https://github.com/mattyork/fuzzy
@@ -18,11 +17,7 @@ const getVisibleItems = createSelector(
       return items
     } else {
       const r = new RegExp(search, 'i')
-      const result = fuzzy.filter('bcn', items, { extract: item => (_.join(_.values(item), ' ')) })
-      const indexes = _.map(result, res => (res.index))
-      const founded = _.filter(items, (itm, idx) => _.indexOf(indexes, idx) !== -1) 
-      console.log(indexes.length, "found", founded)
-      return founded
+      return _.filter(items, itm => ( r.test(_.join(_.values(itm), ' ')) ))
     }
   }
 )
