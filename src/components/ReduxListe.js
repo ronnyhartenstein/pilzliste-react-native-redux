71337d74a3b307c9f29b99b61e780245698c59a2
Ronny Hartenstein <ronny@rh-flow.de>
2016-09-29 21:15:04 +0200

    Suchbegriffe durch Leerzeichen getrennt

--- src/components/ReduxListe.js
+++ src/components/ReduxListe.js
@@ -5,19 +5,27 @@ import { createSelector } from 'reselect'
 import _ from 'lodash'
 
 // Memoized Selectors: http://redux.js.org/docs/recipes/ComputingDerivedData.html
-// Fuzzy Search: https://github.com/mattyork/fuzzy
 
 const getSearch = (state) => state.search
 const getItems = (state) => state.items
 
+// der Memoized Selectors
 const getVisibleItems = createSelector(
   [ getSearch, getItems ],
   (search, items) => {
     if (_.isEmpty(search) || search.length < 3) {
       return items
     } else {
-      const r = new RegExp(search, 'i')
-      return _.filter(items, itm => ( r.test(_.join(_.values(itm), ' ')) ))
+      // Suchbegriffe mit Leerzeichen getrennt..
+      const terms = search.split(' ')
+      const startTime = new Date()
+      const result = _.filter(items, itm => { 
+          const haystack = _.values(itm).join(' ')
+          return terms.map(term => ( haystack.indexOf(term) !== -1 )) // Treffer je Suchbegriff?
+                      .reduce((prev, curr) => ( prev && curr ), true) // alle Suchbegriffe true? 
+      })
+      console.log(`Suche: ${result.length} Treffer in ${new Date() - startTime}ms`)
+      return result
     }
   }
 )
