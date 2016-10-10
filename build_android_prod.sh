07c0a862c0f5ab3378a1e80fc56ce8a411d2c591
Ronny Hartenstein <ronny@rh-flow.de>
2016-10-10 23:10:33 +0200

    Android Build ausprobiert - geht nicht..

--- build_android_prod.sh
+++ build_android_prod.sh
@@ -2,5 +2,7 @@
 
 cd android
 ./gradlew assembleRelease
+
+#cd .. && react-native run-android --variant=release
 jarsigner -verbose -keystore my-keystore.keystore app/build/outputs/apk/app-release-unsigned.apk name_alias
 adb install -r app/build/outputs/apk/app-release-unsigned.apk
