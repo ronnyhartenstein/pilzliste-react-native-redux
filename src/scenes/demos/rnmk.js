b334f1023afe24262552d71d38880584e137e3f2
Ronny Hartenstein <ronny@rh-flow.de>
2016-10-19 21:11:12 +0200

    funktioniert

--- src/scenes/demos/rnmk.js
+++ src/scenes/demos/rnmk.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, ScrollView, Text, StyleSheet, Image } from 'react-native'
 import { Actions } from 'react-native-router-flux'
 import { getTheme, setTheme,
   MKButton, MKColor, 
@@ -18,9 +18,6 @@ const theme = getTheme();
 //   borderOffColor: MKColor.Teal,
 //   rippleColor: `rgba(${MKColor.RGBTeal},.15)`,
 // }});
-
-const styles = StyleSheet.create({
-})
 
 export default class DemoMaterialKitScene extends Component {
   constructor(props) {
@@ -41,10 +38,10 @@ export default class DemoMaterialKitScene extends Component {
     //       .withMaxValue(50)
     //       .build();
 
-    const Textfield = MKTextField.textfield()
-      .withPlaceholder('Text...')
-      .withStyle(styles.textfield)
-      .build();
+    // const Textfield = MKTextField.textfield()
+    //   .withPlaceholder('Text...')
+    //   .withStyle(styles.textfield)
+    //   .build();
 
     const menu = (
       <MKIconToggle
@@ -60,9 +57,8 @@ export default class DemoMaterialKitScene extends Component {
       </MKIconToggle>
     )
 
-    return (
-        <View style={styles.container}>
-          <MKButton
+    const button = (
+        <MKButton
             backgroundColor={MKColor.Teal}
             shadowRadius={2}
             shadowOffset={{width:0, height:2}}
@@ -77,8 +73,10 @@ export default class DemoMaterialKitScene extends Component {
             RAISED BUTTON
           </Text>
         </MKButton>
+    )
 
-        <View style={theme.cardStyle}>
+    const card = (
+      <View style={theme.cardStyle}>
           <Image source={{uri : 'https://pilze.rh-flow.de/thumbnails/Alkalischer%20Saftling.jpg'}} style={theme.cardImageStyle} />
           <Text style={theme.cardTitleStyle}>Welcome</Text>
           <Text style={theme.cardContentStyle}>
@@ -88,44 +86,51 @@ export default class DemoMaterialKitScene extends Component {
           <View style={theme.cardMenuStyle}>{menu}</View>
           <Text style={theme.cardActionStyle}>My Action</Text>
         </View>
+    )
 
-        <MKProgress
+    const progress = (
+      <MKProgress
           style={styles.progress}
           progress={0.2}
         />
+    )
 
-        <MKSpinner />
-{
-        // <mdl.Slider style={styles.slider} />
+    const spinner = (
+      <MKSpinner />
+    )
 
-        // <SliderWithValue
-        //   ref="sliderWithValue"
-        //   onChange={(curValue) => this.setState({curValue})}
-        // />
+    // const sliderWithValue = (
+    //     <SliderWithValue
+    //       ref="sliderWithValue"
+    //       onChange={(curValue) => this.setState({curValue})}
+    //     />
+    // )
 
-        // <mdl.RangeSlider style={styles.slider} />
+    // const sliderWithRange = (
+    //     <SliderWithRange
+    //       ref="sliderWithRange"
+    //       onChange={(curValue) => this.setState({
+    //         min: curValue.min,
+    //         max: curValue.max,
+    //         })
+    //       }
+    //       onConfirm={(curValue) => {
+    //         console.log("Slider drag ended");
+    //         console.log(curValue);
+    //       }}
+    //     />
+    // )
 
-        // <SliderWithRange
-        //   ref="sliderWithRange"
-        //   onChange={(curValue) => this.setState({
-        //     min: curValue.min,
-        //     max: curValue.max,
-        //     })
-        //   }
-        //   onConfirm={(curValue) => {
-        //     console.log("Slider drag ended");
-        //     console.log(curValue);
-        //   }}
-        // />
-}
-        <Textfield />
+    const textfield = (
         <MKTextField
           tintColor={MKColor.Lime}
           textInputStyle={{color: MKColor.Orange}}
           placeholder="Text…"
           style={styles.textfield}
         />
+    )
 
+    const icontoggle = (
         <MKIconToggle
           checked={true}
           onCheckedChange={this._onIconChecked}
@@ -138,7 +143,9 @@ export default class DemoMaterialKitScene extends Component {
                 pointerEvents="none"
                 style={[styles.toggleText, styles.toggleTextOn]}>On</Text>
         </MKIconToggle>
+    )
 
+    const switcho = (
         <MKSwitch
           style={styles.appleSwitch}
           onColor="rgba(255,152,0,.3)"
@@ -147,7 +154,35 @@ export default class DemoMaterialKitScene extends Component {
           onPress={() => console.log('orange switch pressed')}
           onCheckedChange={(e) => console.log('orange switch checked', e)}
         />
-      </View>
+    )
+
+    return (
+        <ScrollView style={styles.container}>
+          <View style={styles.box}>{button}</View>
+          <View style={styles.box}>{card}</View>
+          <View style={styles.box}>{progress}</View>
+          <View style={styles.box}>{spinner}</View>
+          {/*<View style={styles.box}>{sliderWithValue}</View>
+          <View style={styles.box}>{sliderWithRange}</View>*/}
+          <View style={styles.box}>{textfield}</View>
+          <View style={styles.box}>{icontoggle}</View>
+          <View style={styles.box}>{switcho}</View>
+      </ScrollView>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  box: {
+    //height:300
+    borderTopWidth: 1,
+    borderColor: 'black',
+    padding: 10
+  },
+  container: {
+    flex: 1, 
+    flexDirection: 'column',
+    marginTop: 50
+  }
+})
