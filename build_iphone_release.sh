2e08f5adfc8bcc74288ad21057e7a542141a6c5a
Ronny Hartenstein <ronny@rh-flow.de>
2017-01-09 23:17:14 +0100

    Iphone Release build with fastlane gym

---
+++ build_iphone_release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Automating the Build Process
+# https://developer.apple.com/library/content/documentation/DeveloperTools/Conceptual/testing_with_xcode/chapters/08-automation.html
+# http://stackoverflow.com/questions/5010062/xcodebuild-simulator-or-device
+# http://stackoverflow.com/questions/34003723/build-and-run-an-app-on-simulator-using-xcodebuild?rq=1
+
+# Fastlane: https://fastlane.tools
+# Gym: https://github.com/fastlane/fastlane/tree/master/gym
+fastlane gym --scheme "Pilzliste" --clean --silent --export_method development
+
+# Push to device
+# http://stackoverflow.com/questions/14535392/how-to-install-an-ipa-app-file-into-iphone-with-command-line/14652288#14652288
+# Generate and Install IPA's file in device through Command Line: https://gist.github.com/phynet/38afad2170907d2c1ccd
+# ideviceinstaller: http://www.libimobiledevice.org
+
+ideviceinstaller -i Pilzliste.ipa
+
+# Simulator: https://github.com/phonegap/ios-sim
--- build_iphone_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Automating the Test Process
-# https://developer.apple.com/library/content/documentation/DeveloperTools/Conceptual/testing_with_xcode/chapters/08-automation.html
-
-cd ios
-xcodebuild test -project Pilzliste.xcodeproj -scheme Pilzliste -destination 'platform=iOS,name=iPhone von Ronny Hartenstein'
