3197b87c1da681ab45f5d20504b786b1a7a22bb9
Ronny Hartenstein <ronny@rh-flow.de>
2017-11-08 21:19:24 +0100

    Upgrade auf React 16.0.0
    $ react-native-git-upgrade
      git-upgrade info Check for updates
      git-upgrade info Using yarn 1.2.1
      git-upgrade info Read package.json files
      git-upgrade info Check declared version
      git-upgrade info Check matching versions
      git-upgrade info Check React peer dependency
      git-upgrade info Check that Git is installed
      git-upgrade info Get information from NPM registry
      git-upgrade info Upgrading to React Native 0.50.3, React 16.0.0
      git-upgrade info Setup temporary working directory
      git-upgrade info Configure Git environment
      git-upgrade info Init Git repository
      git-upgrade info Add all files to commit
      warning: CRLF will be replaced by LF in android/gradlew.bat.
      The file will have its original line endings in your working directory.
      git-upgrade info Commit current project sources
      git-upgrade info Create a tag before updating sources
      git-upgrade info Generate old version template
      git-upgrade info Add updated files to commit
      git-upgrade info Commit old version template
      git-upgrade info Install the new version
      warning "react-addons-pure-render-mixin@15.0.2" has incorrect peer dependency "react@^15.0.2".
      warning "react-static-container@1.0.1" has incorrect peer dependency "react@^0.13.0 || ^0.14.0 || ^15.0.0".
      warning "react-addons-pure-render-mixin@15.3.2" has incorrect peer dependency "react@^15.3.2".
      warning "react-redux@4.4.5" has incorrect peer dependency "react@^0.14.0 || ^15.0.0-0".
      git-upgrade info Generate new version template
      git-upgrade info Add updated files to commit
      warning: CRLF will be replaced by LF in android/gradlew.bat.
      The file will have its original line endings in your working directory.
      git-upgrade info Commit new version template
      git-upgrade info Generate the patch between the 2 versions
      git-upgrade info Save the patch in temp directory
      git-upgrade info Reset the 2 temporary commits
      git-upgrade info Apply the patch
      error: patch failed: .gitignore:22
      Falling back to three-way merge...
      Applied patch to '.gitignore' cleanly.
      error: patch failed: android/app/src/main/java/com/pilzliste/MainApplication.java:1
      Falling back to three-way merge...
      Applied patch to 'android/app/src/main/java/com/pilzliste/MainApplication.java' with conflicts.
      error: patch failed: ios/Pilzliste.xcodeproj/project.pbxproj:22
      Falling back to three-way merge...
      Applied patch to 'ios/Pilzliste.xcodeproj/project.pbxproj' with conflicts.
      error: patch failed: ios/Pilzliste.xcodeproj/xcshareddata/xcschemes/Pilzliste.xcscheme:3
      Falling back to three-way merge...
      Applied patch to 'ios/Pilzliste.xcodeproj/xcshareddata/xcschemes/Pilzliste.xcscheme' cleanly.
      U android/app/src/main/java/com/pilzliste/MainApplication.java
      U ios/Pilzliste.xcodeproj/project.pbxproj
      git-upgrade WARN The upgrade process succeeded but there might be conflicts to be resolved. See above for the list of files that have merge conflicts.
      git-upgrade info Upgrade done

--- android/app/src/main/java/com/pilzliste/MainApplication.java
+++ android/app/src/main/java/com/pilzliste/MainApplication.java
@@ -1,14 +1,13 @@
 package com.pilzliste;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,7 +16,7 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    protected boolean getUseDeveloperSupport() {
+    public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
 
@@ -28,10 +27,21 @@ public class MainApplication extends Application implements ReactApplication {
             new ReactMaterialKitPackage()
       );
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
   };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
   }
 }
